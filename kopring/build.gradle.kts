92751c0ab92987abf5d14154107a52938b683659
unluckyjung <ybook2012@gmail.com>
2022-04-24 18:22:59 +0900

    chore: jpa를 위한 h2 데이터베이스 설정

--- kopring/build.gradle.kts
+++ kopring/build.gradle.kts
@@ -23,6 +23,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("com.h2database:h2:2.1.210")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

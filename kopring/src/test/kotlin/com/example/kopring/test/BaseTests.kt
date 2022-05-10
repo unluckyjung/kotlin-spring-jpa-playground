329dafe96ef8e021a0b7758e65631c63c31d25d8
unluckyjung <ybook2012@gmail.com>
2022-05-10 23:21:10 +0900

    feat: 통합테스트 어노테이션에 트랜잭셔널 어노테이션 추가

--- kopring/src/test/kotlin/com/example/kopring/test/BaseTests.kt
+++ kopring/src/test/kotlin/com/example/kopring/test/BaseTests.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
+import org.springframework.transaction.annotation.Transactional
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
@@ -24,6 +25,7 @@ annotation class RepositoryTest
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
+@Transactional
 @SpringBootTest
 @TestEnvironment
 annotation class IntegrationTest

4e2197c9300adfad77df00f7a29bbbf17550c52a
unluckyjung <ybook2012@gmail.com>
2022-05-10 22:37:20 +0900

    feat: 테스트관련 어노테이션 구현

---
+++ kopring/src/test/kotlin/com/example/kopring/test/BaseTests.kt
@@ -0,0 +1,29 @@
+package com.example.kopring.test
+
+import io.mockk.junit5.MockKExtension
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestConstructor
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+annotation class TestEnvironment
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@ExtendWith(MockKExtension::class)
+annotation class UnitTest
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@DataJpaTest
+@TestEnvironment
+annotation class RepositoryTest
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@SpringBootTest
+@TestEnvironment
+annotation class IntegrationTest

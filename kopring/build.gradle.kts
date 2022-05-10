ab9e2e03c2ba17bdb953fd90d57fed6c2084ce03
unluckyjung <ybook2012@gmail.com>
2022-05-11 00:51:27 +0900

    chore: mockk 관련 의존성 추가

--- kopring/build.gradle.kts
+++ kopring/build.gradle.kts
@@ -36,6 +36,9 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:5.2.3")
     testImplementation("io.kotest:kotest-assertions-core:5.2.3")
     testImplementation("io.kotest:kotest-property:5.2.3")
+    testImplementation("io.mockk:mockk:1.12.3")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
+
 }
 
 tasks.withType<KotlinCompile> {

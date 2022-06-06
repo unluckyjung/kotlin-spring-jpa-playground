accbbcf025ca8428656f19e0ce23d631e3ae8fa5
unluckyjung <ybook2012@gmail.com>
2022-06-06 14:39:10 +0900

    feat: kotlin 어노테이션을 이용한 어노테이션 달리는 위치 조정 테스트

--- kopring/src/main/kotlin/com/example/kopring/member/domain/Member.kt
+++ kopring/src/main/kotlin/com/example/kopring/member/domain/Member.kt
@@ -3,6 +3,7 @@ package com.example.kopring.member.domain
 import com.example.kopring.BaseEntity
 import java.time.ZonedDateTime
 import javax.persistence.*
+import javax.validation.constraints.NotBlank
 
 @Entity
 class Member(
@@ -12,4 +13,9 @@ class Member(
     @Column(name = "member_id")
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
-) : BaseEntity(createdAt = createdAt)
+) : BaseEntity(createdAt = createdAt) {
+    data class Request(
+        @field: NotBlank(message = "이름은 공백으로 이루어져있을 수 없습니다.")
+        val name: String
+    )
+}
--- kopring/src/test/kotlin/com/example/kopring/member/domain/MemberTest.kt
+++ kopring/src/test/kotlin/com/example/kopring/member/domain/MemberTest.kt
@@ -1,5 +1,41 @@
 package com.example.kopring.member.domain
 
-import org.junit.jupiter.api.Assertions.*
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldMatch
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import javax.validation.Validation
 
-internal class MemberTest
+class MemberTest {
+
+    @DisplayName("Request 테스트")
+    @Nested
+    class RequestTest {
+        private val validator = Validation.buildDefaultValidatorFactory().validator
+
+        @DisplayName("이름이 공백이면 Validation 의 validator 에 예외가 쌓인다.")
+        @Test
+        fun nameBlankFailTest() {
+
+            val memberReq = Member.Request(" ")
+            val violations = validator.validate(memberReq)
+
+            violations.size shouldBe 1
+
+            for (failMsg in violations) {
+                failMsg.message shouldMatch ("이름은 공백으로 이루어져있을 수 없습니다.")
+            }
+        }
+
+        @DisplayName("이름이 공백으로 이루어있지 않으면, 예외가 쌓이지 않는다.")
+        @Test
+        fun nameBlankTest() {
+
+            val memberReq = Member.Request("jys")
+            val violations = validator.validate(memberReq)
+
+            violations.size shouldBe 0
+        }
+    }
+}

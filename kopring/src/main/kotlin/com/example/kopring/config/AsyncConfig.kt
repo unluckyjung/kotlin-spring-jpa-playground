e1043542285dbdf287de6b6a62358735f057aa2a
unluckyjung <ybook2012@gmail.com>
2022-07-17 23:37:57 +0900

    feat: 반환값이 있는 경우 예시 추가

--- kopring/src/main/kotlin/com/example/kopring/config/AsyncConfig.kt
+++ kopring/src/main/kotlin/com/example/kopring/config/AsyncConfig.kt
@@ -2,7 +2,6 @@ package com.example.kopring.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
--- kopring/src/test/kotlin/com/example/kopring/common/SpringAsyncTest.kt
+++ kopring/src/test/kotlin/com/example/kopring/common/SpringAsyncTest.kt
@@ -4,7 +4,10 @@ import com.example.kopring.test.IntegrationTest
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.AsyncResult
 import org.springframework.stereotype.Service
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
 
 @IntegrationTest
 class SpringAsyncTest(
@@ -13,14 +16,40 @@ class SpringAsyncTest(
     @Test
     fun asyncTest() {
         dummyService.asyncFun()
-        dummyService.nonAsyncFun()
+        dummyService.syncFun()
     }
 
     @Test
     fun nonAsyncTest() {
         // 전부 순차적으로 로깅
-        dummyService.nonAsyncFun()
-        dummyService.nonAsyncFun()
+        dummyService.syncFun()
+        dummyService.syncFun()
+    }
+
+    @Test
+    fun asyncStringReturnTest() {
+        for (i in 1..10) {
+            println(dummyService.asyncFunString(i))
+        }
+
+        for (i in 1..10) {
+            println(dummyService.syncFunString(i))
+        }
+
+        for (i in 1..10) {
+            val asyncFunStringFuture = dummyService.asyncFunStringFuture(i)
+            println(asyncFunStringFuture)
+
+            // 결과를 얻을때까지 블로킹
+            println(asyncFunStringFuture.get())
+        }
+
+        for (i in 1..10) {
+            val asyncFunStringFuture = dummyService.asyncFunStringFuture2(i)
+
+            // asyncFunStringFuture.isDone // isDone 을 이용해 완료된 뒤에 호출할 수 도 있음.
+            println(asyncFunStringFuture.get())
+        }
     }
 }
 
@@ -35,12 +64,33 @@ class DummyService {
         }
     }
 
-    fun nonAsyncFun() {
+    fun syncFun() {
         for (num in 1..10) {
             logger.info("NonAsyncFun: $num")
         }
     }
 
+    @Async("threadPoolTaskExecutor")
+    fun asyncFunString(num: Int): String {
+        return "asyncFunString: $num"
+    }
+
+    // 두가지의 방법중 어떤것이 general 한 방식인지는 모르겠음
+
+    @Async("threadPoolTaskExecutor")
+    fun asyncFunStringFuture(num: Int): CompletableFuture<String> {
+        return CompletableFuture.completedFuture("asyncFunString: $num")
+    }
+
+    // ListenableFuture 상속 형태
+    @Async("threadPoolTaskExecutor")
+    fun asyncFunStringFuture2(num: Int): Future<String> {
+        return AsyncResult("asyncFunString[2]: $num")
+    }
+
+    fun syncFunString(num: Int): String {
+        return "syncFunString: $num"
+    }
 
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)

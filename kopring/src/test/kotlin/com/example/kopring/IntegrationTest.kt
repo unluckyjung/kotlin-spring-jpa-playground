2c06d6e60b43b473f047758915e725936ec73e83
goodall <goodall@dunamu.com>
2022-07-28 19:19:48 +0900

    feat: 리소스락 부분 추가

--- kopring/src/test/kotlin/com/example/kopring/IntegrationTest.kt
+++ kopring/src/test/kotlin/com/example/kopring/IntegrationTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+// @ResourceLock(value = "resources") // 해당 옵션을 주면 클래스내 테스트끼리는 하나의 스레드에서 작동
 class IntegrationTest {
     @Test
     fun test1() {

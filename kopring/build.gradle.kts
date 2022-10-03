b694a20cd6c04d2f352c06988282b30b412c60ed
unluckyjung <ybook2012@gmail.com>
2022-10-03 20:58:17 +0900

    chore: jpa 관련 allopen 플러그인 추가

--- kopring/build.gradle.kts
+++ kopring/build.gradle.kts
@@ -16,6 +16,12 @@ repositories {
     mavenCentral()
 }
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")

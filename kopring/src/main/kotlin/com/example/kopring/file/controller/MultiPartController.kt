f4456d2bdf4e4ffc97b30cd4a2f39e87cafa6486
unluckyjung <ybook2012@gmail.com>
2022-10-18 02:57:46 +0900

    feat: json 을 구분해 따로 받지 않고 한번에 처리하는 방식 추가

--- kopring/src/main/kotlin/com/example/kopring/file/controller/MultiPartController.kt
+++ kopring/src/main/kotlin/com/example/kopring/file/controller/MultiPartController.kt
@@ -19,9 +19,38 @@ class MultiPartController(
     ) {
         fileService.upload(id, file, dto, filename)
     }
+
+    // file-upload2 에서 따로따로 다 처리하는 방식
+//    @PostMapping("/{id}/attribute")
+//    fun upload2(
+//        @PathVariable id: Long,
+//        @RequestPart file: MultipartFile,
+//        @RequestPart name: String,
+//        @RequestPart age: String,
+//        @RequestParam filename: String
+//    ) {
+//        println(id)
+//    }
+
+    // file-upload2 에서 보내는걸 한번에 받아서 처리하는 방식
+    @PostMapping("/{id}/attribute")
+    fun upload3(
+        @PathVariable id: Long,
+        @ModelAttribute dto: EntireDto,
+        @RequestParam filename: String
+    ) {
+        println(id)
+//        fileService.upload(id, file, dto, filename)
+    }
 }
 
 data class RequestDto(
     val name: String,
     val age: Int,
+)
+
+data class EntireDto(
+    val file: MultipartFile,
+    val name: String,
+    val age: String,
 )

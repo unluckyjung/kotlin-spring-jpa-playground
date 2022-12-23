ed11b612fc6f81a274ba10b630845d31ae630e90
goodall <goodall@dunamu.com>
2022-12-23 19:53:23 +0900

    fix: mocking 이 되는 형태로 변경

--- kopring/src/main/kotlin/com/example/kopring/common/support/jpa/CustomCrudRepository.kt
+++ kopring/src/main/kotlin/com/example/kopring/common/support/jpa/CustomCrudRepository.kt
@@ -1,8 +1,8 @@
 package com.example.kopring.common.support.jpa
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
 
-
-inline fun <reified T, ID : Any> CrudRepository<T, ID>.findByIdOrThrow(id: ID, e: Exception? = null): T =
-    findById(id).orElseThrow { e ?: IllegalArgumentException("${T::class.java.name} entity 를 찾을 수 없습니다. id=$id") }
-
+inline fun <reified T, ID> CrudRepository<T, ID>.findByIdOrThrow(
+    id: ID, e: Exception = IllegalArgumentException("${T::class.java.name} entity 를 찾을 수 없습니다. id=$id")
+): T = findByIdOrNull(id) ?: throw e

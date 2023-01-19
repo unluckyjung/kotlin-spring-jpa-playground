53ba0b9273434a4e15efc9b3973e90b836b7ab2d
unluckyjung <ybook2012@gmail.com>
2023-01-20 03:16:21 +0900

    feat: object 를 Key 로도 사용가능한 예시 추가

--- kopring/src/test/kotlin/com/example/kopring/common/CacheTest.kt
+++ kopring/src/test/kotlin/com/example/kopring/common/CacheTest.kt
@@ -3,6 +3,7 @@ package com.example.kopring.common
 import com.example.kopring.test.IntegrationTest
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.CachePut
@@ -15,27 +16,62 @@ class CacheTest(
     val dummyService: DummyService
 ) {
 
+    @DisplayName("캐싱된 결과는 같은 해시코드를 출력한다.")
     @Test
     fun cacheTest() {
 
         val cachedValue = dummyService.cachedFun("goodall")
         val nonCachedValue = dummyService.nonCachedFun("jys")
 
-//        println("first hash: ${cachedValue.hashCode()}")
-//        println("first hash: ${nonCachedValue.hashCode()}")
+        println("first hash: ${cachedValue.hashCode()}")
+        println("first hash: ${nonCachedValue.hashCode()}")
 
         for (i in 1..3) {
             cachedValue shouldBe dummyService.cachedFun("goodall")
             nonCachedValue shouldNotBe dummyService.nonCachedFun("jys")
 
-//            println("hash: ${dummyService.cachedFun("goodall").hashCode()}")
-//            println("hash: ${dummyService.nonCachedFun("jys").hashCode()}")
+            println("hash: ${dummyService.cachedFun("goodall").hashCode()}")
+            println("hash: ${dummyService.nonCachedFun("jys").hashCode()}")
+        }
+    }
+
+    @DisplayName("캐싱 key 가 객체여도 캐싱된 결과는 같은 해시코드를 출력한다.")
+    @Test
+    fun cacheObjectTest() {
+        val req = Request("goodall", 31)
+        val cachedValue = dummyService.cachedObjectFun(req)
+
+        println("first hash: ${cachedValue.hashCode()}")
+
+        for (i in 1..3) {
+            cachedValue shouldBe dummyService.cachedObjectFun(req)
+            cachedValue shouldNotBe Request("goodall", 31)
+            println("hash: ${dummyService.cachedObjectFun(req).hashCode()}")
+        }
+        Counter.count shouldBe 1
+
+        // new Request
+        val newReq = Request("jys", 31)
+        println("hash: ${dummyService.cachedObjectFun(newReq).hashCode()}")
+        Counter.count shouldBe 2
+    }
+
+    @DisplayName("[캐싱키 명시] 캐싱 key 가 객체여도 캐싱된 결과는 같은 해시코드를 출력한다.")
+    @Test
+    fun cacheObjectTest2() {
+        val req = Request("goodall", 31)
+        val cachedValue = dummyService.cachedObjectFun2(req)
+
+        println("first hash: ${cachedValue.hashCode()}")
+
+        for (i in 1..3) {
+            cachedValue shouldBe dummyService.cachedObjectFun2(req)
+            println("hash: ${dummyService.cachedObjectFun2(req).hashCode()}")
         }
     }
 
     @Test
     fun cacheTest2() {
-
         val cachedValue = dummyService.cachedFun2("jys", 30, "goodall")
         cachedValue shouldBe dummyService.cachedFun2("jys", 30, "goodall")
     }
@@ -88,6 +124,18 @@ class DummyService {
         return Response(name)
     }
 
+    @Cacheable(cacheNames = ["cacheTest"]) // object 로 넘겨도 캐싱된다.
+    fun cachedObjectFun(req: Request): Response {
+        Counter.countUp()
+        return Response(req.name)
+    }
+
+    @Cacheable(cacheNames = ["cacheTest"], key = "#req") // key 값을 몇시하고, object 로 넘겨도 캐싱된다.
+    fun cachedObjectFun2(req: Request): Response {
+        Counter.countUp()
+        return Response(req.name)
+    }
+
     @Cacheable(value = ["cacheTest2"], key = "#name + #age")
     fun cachedFun2(name: String, age: Int, nickName: String): Response {
         return Response(name)
@@ -130,5 +178,15 @@ class Response(
 )
 
 class Request(
-    val name: String
+    val name: String,
+    val age: Int,
 )
+
+object Counter {
+    var count = 0
+        private set
+
+    fun countUp() {
+        count++
+    }
+}

a6be087b1f4218b0b61538716909f556e963d61f
unluckyjung <ybook2012@gmail.com>
2023-02-06 22:15:00 +0900

    feat: ModelAttribute null 처리 방법

---
+++ kopring/src/main/kotlin/com/example/kopring/member/controller/BasicController.kt
@@ -0,0 +1,43 @@
+package com.example.kopring.member.controller
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("api/v1")
+class BasicController {
+
+    @GetMapping("/get-param")
+    fun getController1(
+        @RequestParam(name = "age") age: Int,
+        @RequestParam(name = "name", defaultValue = "defaultName", required = false) name: String,
+    ): String {
+        return "name: $name"
+    }
+
+    @GetMapping("/get-model")
+    fun getController2(
+        @ModelAttribute req: ModelDto
+    ): String {
+        return "name: ${req.name}"
+    }
+
+    @GetMapping("/wrong-get-model")
+    fun wrongGetController2(
+        @ModelAttribute req: WrongModelDto
+    ): String {
+        // name 에 null 이 명시적으로 차버림. (name 은 NotNull type)
+        return "name: ${req.name}"
+    }
+}
+
+class ModelDto(
+    val age: Int,
+    name: String?,
+) {
+    val name = name ?: "defaultName"
+}
+
+class WrongModelDto(
+    val age: Int,
+    val name: String = "defaultName",
+)

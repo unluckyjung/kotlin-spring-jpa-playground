9ae7d2e28adf14843b4ef272d1aa81c9c4f9751c
yoonsung.jung (Goodall) <ybook2012@gmail.com>
2023-05-21 15:26:53 +0900

    feat: 스프링 이벤트 리스너 사용 예시 구현 (#6)

--- kopring/src/main/kotlin/com/example/kopring/member/service/KtFactoryMethodPattern.kt
+++ kopring/src/main/kotlin/com/example/kopring/member/service/KtFactoryMethodPattern.kt
@@ -24,3 +24,22 @@ class KtFactoryMethodPattern private constructor(
         }
     }
 }
+
+@Component
+class KtFactoryMethodPattern2 private constructor(
+    private val memberRepository: MemberRepository,
+) {
+    init {
+        KtFactoryMethodPattern2.memberRepository = memberRepository
+    }
+
+    companion object {
+        // 의존성 주입 받아야 하는 종류를 이렇게 처리가능
+        private lateinit var memberRepository: MemberRepository
+
+        fun getMemberList(): MutableList<Member> {
+            return memberRepository.findAll()
+        }
+    }
+}
+
--- kopring/src/main/kotlin/com/example/kopring/member/service/application/MemberSave.kt
+++ kopring/src/main/kotlin/com/example/kopring/member/service/application/MemberSave.kt
@@ -1,9 +1,14 @@
 package com.example.kopring.member.service.application
 
+import com.example.kopring.common.support.jpa.findByIdOrThrow
 import com.example.kopring.member.domain.Member
 import com.example.kopring.member.domain.MemberRepository
+import com.example.kopring.member.event.DomainEventPublisher
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
 
 @Transactional
 @Component
@@ -11,6 +16,31 @@ class MemberSave(
     private val memberRepository: MemberRepository,
 ) {
     fun save(req: Member.Request): Member {
-        return Member(name = req.name).let(memberRepository::save)
+        val member = Member(name = req.name).let(memberRepository::save)
+
+        // member 가 저장했을때 이벤트 발생
+        DomainEventPublisher.publishEvent(
+            MemberSaveEvent(memberId = member.id)
+        )
+
+        return member
+    }
+}
+
+data class MemberSaveEvent(
+    val memberId: Long,
+)
+
+@Component
+class MemberSaveEventHandler(
+    private val memberRepository: MemberRepository,
+) {
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)  // 앞 작업에서의 커밋이 종료된뒤 처리
+    @Transactional(propagation = Propagation.REQUIRES_NEW)  // 앞에서 트랜잭션이 완료되어 버렸기 떄문에, 여기서 변경작업을 하려면 새로운 트랜잭션을 열어야함.
+    fun onSave(memberSaveEvent: MemberSaveEvent) {
+
+        memberRepository.findByIdOrThrow(memberSaveEvent.memberId).run {
+            this.changeName(name = "${this.name} event")
+        }
     }
 }

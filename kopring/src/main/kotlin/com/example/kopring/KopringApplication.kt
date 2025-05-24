80942d36c4035160ad9b7fd01722230d3e10786f
yoonsung.jung (Goodall) <ybook2012@gmail.com>
2025-05-24 16:24:51 +0900

    conf: mysql 8.0 으로 변경 (#14)
    * conf: infra pakage 위치 수정
    
    * conf: docker-compose 업데이트
    
    * conf: mysql, h2 프로파일분리

--- kopring/src/main/kotlin/com/example/kopring/KopringApplication.kt
+++ kopring/src/main/kotlin/com/example/kopring/KopringApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+// @ActiveProfiles("mysql") // mysql 로 띄우고 싶은경우 활성화.
 @EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
--- kopring/src/test/kotlin/com/example/kopring/KopringApplicationTests.kt
+++ kopring/src/test/kotlin/com/example/kopring/KopringApplicationTests.kt
@@ -3,6 +3,7 @@ package com.example.kopring
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
+// @ActiveProfiles("mysql") // mysql 로 띄우고 싶은경우 활성화.
 @SpringBootTest
 class KopringApplicationTests {
 
